9af1f042efcdb617421c0e6bb76f92008a761e8e
Molly TM <mollyt@codeforamerica.org>
2019-09-30 14:51:54 -0700

    Parse events into court cases
    - History knows about court cases as well as individual events
    
    [#168842872]

--- models/history.rb
+++ models/history.rb
@@ -1,4 +1,4 @@
-History = Struct.new(:person_name, :ir_number, :dob, :events, keyword_init: true) do
+History = Struct.new(:person_name, :ir_number, :dob, :events, :court_cases, keyword_init: true) do
   def has_pending_case?
     events.each do |event|
       if event.pending_case?
--- spec/parsers/champaign_county_parser_spec.rb
+++ spec/parsers/champaign_county_parser_spec.rb
@@ -123,8 +123,73 @@ RSpec.describe ChampaignCountyParser do
     end
   end
 
+  describe 'parse_court_cases' do
+    let(:row1) {Event.new(
+      arresting_agency_code: 'Toon Town Sheriff',
+      case_number: '2007-CM-000747',
+      charge_code: '720 5/21-1(1)(a)',
+      charge_description: 'Knowingly Damage Prop<$300',
+      offense_type: 'M',
+      offense_class: 'A',
+      disposition_date: '18-Sep-2007',
+      disposition: 'Guilty',
+      sentence: 'Fines and/or Cost/Penalties and Fees; Probation (24 months); Anti-Crime Assessment Fee;;',
+      )}
+
+    let(:row2) {Event.new(
+      arresting_agency_code: 'Toon Town Sheriff',
+      case_number: '2007-CM-000747',
+      charge_code: nil,
+      charge_description: 'Criminal Trespass To Real Prop',
+      offense_type: 'M',
+      offense_class: 'B',
+      disposition_date: '18-Sep-2007',
+      disposition: 'Dismissed',
+      sentence: 'No Sentence',
+      )}
+
+    let(:row3) {Event.new(
+      arresting_agency_code: 'Toon Town Sheriff',
+      case_number: '2013-CM-000748',
+      charge_code: '720 5/21-1(1)(a)',
+      charge_description: 'Knowingly Damage Prop<$50',
+      offense_type: 'M',
+      offense_class: 'C',
+      disposition_date: '18-Sep-2013',
+      disposition: 'Not Guilty',
+      sentence: 'No Sentence',
+      )}
+
+    let(:row4) {Event.new(
+      arresting_agency_code: 'Toon Town Sheriff',
+      case_number: nil,
+      charge_code: nil,
+      charge_description: 'Criminal Mischief',
+      offense_type: nil,
+      offense_class: nil,
+      disposition_date: nil,
+      disposition: nil,
+      sentence: nil,
+      )}
+    it 'creates a court case for each case number' do
+      court_cases = subject.parse_court_cases([row1, row2, row3, row4])
+
+      expect(court_cases.length).to eq(2)
+      expect(court_cases[0].case_number).to eq('2007-CM-000747')
+      expect(court_cases[1].case_number).to eq('2013-CM-000748')
+    end
+
+    it 'assigns all court events with a given case number to that court case' do
+      court_cases = subject.parse_court_cases([row1, row2, row3, row4])
+
+      expect(court_cases.length).to eq(2)
+      expect(court_cases[0].events.length).to eq(2)
+      expect(court_cases[1].events.length).to eq(1)
+    end
+  end
+
   describe 'parse_history' do
-    it 'returns a History that contains an Event of each row provided' do
+    it 'returns a History that contains an Event of each row provided and corresponding CourtCases' do
       row1 = {
         individual: 'CHIPMUNK, ALVIN',
         date_of_birth: '02-May-85',
@@ -147,11 +212,11 @@ RSpec.describe ChampaignCountyParser do
         individual: 'CHIPMUNK, ALVIN',
         date_of_birth: '02-May-85',
         arresting_agency: nil,
-        case_number: '2009-CM-000747',
+        case_number: '2007-CM-000747',
         status_description: 'Closed',
         date_filed: '05-Jul-2009',
         charge: '- Criminal Trespass To Real Prop - Class: B',
-        disposition_date: '18-Sep-2009',
+        disposition_date: '18-Sep-2007',
         disposition: 'Dismissed',
         sentence: 'No Sentence',
         balance: '$0.00',
@@ -167,6 +232,9 @@ RSpec.describe ChampaignCountyParser do
       expect(history.dob).to eq('02-May-85')
       expect(history.events.length).to eq(2)
       expect(history.events.first.class).to eq(Event)
+      expect(history.court_cases.length).to eq(1)
+      expect(history.court_cases.first.class).to eq(CourtCase)
+      expect(history.court_cases.first.case_number).to eq('2007-CM-000747')
     end
   end
 end

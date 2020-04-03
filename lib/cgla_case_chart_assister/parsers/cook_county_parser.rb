b5022b3598690e13b68c3131cc31876f0335f4d1
Symonne Singleton <symonne@codeforamerica.org>
2020-04-03 16:49:30 -0700

    Finish Cook Parser
    Signed-off-by: Christa Hartsock <christa@codeforamerica.org>
    Co-authored-by: Christa Hartsock <christa@codeforamerica.org>

--- lib/cgla_case_chart_assister/parsers/cook_county_parser.rb
+++ lib/cgla_case_chart_assister/parsers/cook_county_parser.rb
@@ -7,33 +7,47 @@ require 'date'
 
 module CglaCaseChartAssister
   class CookCountyParser
-    def parse_event(charge_events, case_number)
-      charge_event = charge_events.first
-      offense_info = parse_offense_classification(charge_event['Degree'])
-      Charge.new(
-          index: charge_event['Sequence'],
-          case_number: case_number,
-          dcn: charge_event['DCN'],
-          code: charge_event['Statute'],
-          description: charge_event['Charge'],
-          offense_type: offense_info[:type],
-          offense_class: offense_info[:class],
-          dispositions: parse_dispositions(charge_events, case_number)
-      )
-    end
-
     def parse_history(history_json)
+      case_details = history_json['CaseDetails']
+
+      court_cases = parse_court_cases(case_details)
+      all_charges = court_cases.map(&:charges).flatten
+
       History.new(
-          person_name: parse_name(history_json['CaseDetails'][0]['Name']),
-          ir_number: history_json['IR_number'],
-          dob: Date.parse(history_json['CaseDetails'][0]['Date_of_Birth']),
-          events: [],
-          court_cases: []
+        person_name: parse_name(case_details[0]['Name']),
+        ir_number: history_json['IR_number'],
+        dob: Date.parse(case_details[0]['Date_of_Birth']),
+        events: all_charges,
+        court_cases: court_cases
       )
     end
 
     def parse_court_cases(case_details_array)
+      case_details_array.map do |case_details|
+        CourtCase.new(
+          case_number: case_details['Case_Number'],
+          charges: parse_charges(case_details['ChargeDetails'], case_number: case_details['Case_Number'])
+        )
+      end
+    end
 
+    def parse_charges(charge_details, case_number:)
+      grouped_details = charge_details.group_by{|charge| charge['Sequence']}
+      grouped_details.map do |sequence_id, charge_details_for_sequence|
+        reference_charge = charge_details_for_sequence.first
+        offense_info = parse_offense_classification(reference_charge['Degree'])
+
+        Charge.new(
+          index: sequence_id,
+          case_number: case_number,
+          dcn: reference_charge['DCN'],
+          code: reference_charge['Statute'],
+          description: reference_charge['Charge'],
+          offense_type: offense_info[:type],
+          offense_class: offense_info[:class],
+          dispositions: parse_dispositions(charge_details_for_sequence, case_number)
+        )
+      end
     end
 
     private
@@ -54,12 +68,12 @@ module CglaCaseChartAssister
       dispositions = []
       charge_events.each do |charge_event|
         dispositions << Disposition.new(
-            case_number: case_number,
-            charge_index: charge_event['Sequence'],
-            description: charge_event['Disposition_description'],
-            date: Date.parse(format_disposition_date(charge_event['Disposition_date'])),
-            sentence_description: charge_event['Sentence'],
-            sentence_duration: charge_event['Sentence_duration']
+          case_number: case_number,
+          charge_index: charge_event['Sequence'],
+          description: charge_event['Disposition_description'],
+          date: Date.parse(format_disposition_date(charge_event['Disposition_date'])),
+          sentence_description: charge_event['Sentence'],
+          sentence_duration: charge_event['Sentence_duration']
         )
       end
       dispositions
@@ -74,3 +88,4 @@ module CglaCaseChartAssister
     end
   end
 end
+
--- spec/lib/cgla_case_chart_assister/parsers/cook_county_parser_spec.rb
+++ spec/lib/cgla_case_chart_assister/parsers/cook_county_parser_spec.rb
@@ -6,6 +6,7 @@ require 'date'
 RSpec.describe CglaCaseChartAssister::CookCountyParser do
   let(:subject) { CglaCaseChartAssister::CookCountyParser.new }
   let(:cook_json) { JSON.parse(FixtureHelper.read_fixture('./spec/fixtures/cook_county.json')) }
+
   describe 'parse_history' do
     it 'returns a History that contains an Event of each analyzable row provided and corresponding CourtCases' do
       history = subject.parse_history(cook_json)
@@ -21,183 +22,6 @@ RSpec.describe CglaCaseChartAssister::CookCountyParser do
     end
   end
 
-  describe 'parse_event' do
-    let(:charge_json) { [{
-                             "DCN" => "0123456789",
-                             "Statute" => "720-570/402(C)",
-                             "Charge" => "POSS AMT CON SUB EXCEPT(A)/(D)",
-                             "Sentence" => "Probation",
-                             "Sequence" => "6",
-                             "Degree" => "Class 4 Felony",
-                             "Disposition_description" => "Finding of Guilty",
-                             "Disposition_date" => "10/18/2000 12:00:00 AM",
-                             "Sentence_duration" => "0"
-                         },
-                         {
-                             "DCN" => "0123456789",
-                             "Statute" => "720-570/402(C)",
-                             "Charge" => "POSS AMT CON SUB EXCEPT(A)/(D)",
-                             "Sentence" => "Probation",
-                             "Sequence" => "6",
-                             "Degree" => "Class 4 Felony",
-                             "Disposition_description" => "Plea of Guilty",
-                             "Disposition_date" => "10/18/2000 12:00:00 AM",
-                             "Sentence_duration" => "0"
-                         },
-                         {
-                             "DCN" => "0123456789",
-                             "Statute" => "720-570/402(C)",
-                             "Charge" => "POSS AMT CON SUB EXCEPT(A)/(D)",
-                             "Sentence" => "Probation",
-                             "Sequence" => "6",
-                             "Degree" => "Class 4 Felony",
-                             "Disposition_description" => "Probation Terminated- Satisfactory",
-                             "Disposition_date" => "10/18/2000 12:00:00 AM",
-                             "Sentence_duration" => "0"
-                         }] }
-    it 'builds a Charge using the data from the history json and a case number' do
-      event = subject.parse_event(charge_json, '123')
-
-      expect(event.type).to eq(:charge)
-      expect(event.index).to eq('6')
-      expect(event.central_booking_number).to eq(nil)
-      expect(event.case_number).to eq('123')
-      expect(event.date_filed).to eq(nil)
-      expect(event.arresting_agency_code).to eq(nil)
-      expect(event.dcn).to eq('0123456789')
-      expect(event.code).to eq('720-570/402(C)')
-      expect(event.description).to eq('POSS AMT CON SUB EXCEPT(A)/(D)')
-      expect(event.offense_type).to eq('Felony')
-      expect(event.offense_class).to eq('4')
-      expect(event.dispositions.count).to eq(3)
-      expect(event.dispositions.first.description).to eq('Finding of Guilty')
-      expect(event.dispositions.first.date).to eq(Date.parse('2000-10-18'))
-      expect(event.dispositions.first.sentence_description).to eq('Probation')
-      expect(event.dispositions.first.sentence_duration).to eq('0')
-    end
-
-    xcontext 'when charge event data does not match'
-
-    xcontext 'when degree does not have a class'
-
-    xcontext 'when an event has no case number' do
-      it 'builds a Arrest using the data from a csv row and an index' do
-        fake_row = {
-            individual: 'CHIPMUNK, ALVIN',
-            date_of_birth: '02-May-85',
-            police_agency: 'Toon County Sheriff',
-            case_number: nil,
-            dcn: 'L6700000',
-            date_filed: '05-Jul-2007',
-            charge: 'Knowingly Damage Prop<$300',
-            disposition_date: nil,
-            disposition: nil,
-            sentence: nil,
-            balance: '$0.00',
-            conviction: nil,
-            eligibility: nil,
-            wp: nil,
-            notes: nil
-        }
-        event = subject.parse_event(fake_row, 3)
-
-        expect(event.type).to eq(:arrest)
-        expect(event.index).to eq(3)
-        expect(event.central_booking_number).to eq(nil)
-        expect(event.case_number).to eq(nil)
-        expect(event.date_filed).to eq('05-Jul-2007')
-        expect(event.arresting_agency_code).to eq('Toon County Sheriff')
-        expect(event.dcn).to eq('L6700000')
-        expect(event.code).to eq(nil)
-        expect(event.description).to eq('Knowingly Damage Prop<$300')
-        expect(event.offense_type).to eq(nil)
-        expect(event.offense_class).to eq(nil)
-        expect(event.dispositions).to eq(nil)
-      end
-    end
-
-    xcontext 'when the charge code is missing' do
-      it 'correctly parses the other charge elements' do
-        fake_row = {
-            individual: 'CHIPMUNK, ALVIN',
-            date_of_birth: '02-May-85',
-            arresting_agency: nil,
-            case_number: '2007-CM-000747',
-            date_filed: '05-Jul-2007',
-            charge: '- Criminal Trespass To Real Prop - Class: B',
-            disposition_date: '18-Sep-2007',
-            disposition: 'Guilty',
-            sentence: 'Fines and/or Cost/Penalties and Fees; Probation (24 months); Anti-Crime Assessment Fee;;',
-            balance: '$0.00',
-            conviction: nil,
-            eligibility: nil,
-            wp: nil,
-            notes: nil
-        }
-        event = subject.parse_event(fake_row, 1)
-
-        expect(event.code).to eq('')
-        expect(event.description).to eq('Criminal Trespass To Real Prop')
-        expect(event.offense_type).to eq('M')
-        expect(event.offense_class).to eq('B')
-      end
-    end
-
-    xcontext 'when the offense class is missing' do
-      it 'correctly parses the other charge elements and leaves offense type blank' do
-        fake_row = {
-            individual: 'CHIPMUNK, ALVIN',
-            date_of_birth: '02-May-85',
-            arresting_agency: nil,
-            case_number: '2007-CM-000747',
-            date_filed: '05-Jul-2007',
-            charge: '720 5/21-1(1)(a) - Knowingly Damage Prop<$300 - Class:',
-            disposition_date: '18-Sep-2007',
-            disposition: 'Guilty',
-            sentence: 'Fines and/or Cost/Penalties and Fees; Probation (24 months); Anti-Crime Assessment Fee;;',
-            balance: '$0.00',
-            conviction: nil,
-            eligibility: nil,
-            wp: nil,
-            notes: nil
-        }
-        event = subject.parse_event(fake_row, 1)
-
-        expect(event.code).to eq('720 5/21-1(1)(a)')
-        expect(event.description).to eq('Knowingly Damage Prop<$300')
-        expect(event.offense_type).to eq(nil)
-        expect(event.offense_class).to eq(nil)
-      end
-    end
-
-    xcontext 'when there is only a charge description' do
-      it 'correctly parses the description and leaves the other fields blank' do
-        fake_row = {
-            individual: 'CHIPMUNK, ALVIN',
-            date_of_birth: '02-May-85',
-            arresting_agency: nil,
-            case_number: '2007-CM-000747',
-            date_filed: '05-Jul-2007',
-            charge: 'Knowingly Damage Prop<$300',
-            disposition_date: '18-Sep-2007',
-            disposition: 'Guilty',
-            sentence: 'Fines and/or Cost/Penalties and Fees; Probation (24 months); Anti-Crime Assessment Fee;;',
-            balance: '$0.00',
-            conviction: nil,
-            eligibility: nil,
-            wp: nil,
-            notes: nil
-        }
-        event = subject.parse_event(fake_row, 1)
-
-        expect(event.code).to eq(nil)
-        expect(event.description).to eq('Knowingly Damage Prop<$300')
-        expect(event.offense_type).to eq(nil)
-        expect(event.offense_class).to eq(nil)
-      end
-    end
-  end
-
   describe 'parse_court_cases' do
     let(:case_details) { cook_json['CaseDetails'] }
     it 'creates a court case for each case number' do
@@ -205,14 +29,47 @@ RSpec.describe CglaCaseChartAssister::CookCountyParser do
 
       expect(court_cases.length).to eq(3)
       expect(court_cases[0].case_number).to eq('00CR1234567')
+      expect(court_cases[0].charges.length).to eq(2)
       expect(court_cases[1].case_number).to eq('14CR0123456')
+      expect(court_cases[1].charges.length).to eq(2)
       expect(court_cases[2].case_number).to eq('95CR0127401')
+      expect(court_cases[2].charges.length).to eq(1)
     end
+  end
 
-    # it 'assigns all court events with a given case number to that court case' do
-    #   expect(court_cases.length).to eq(3)
-    #   expect(court_cases[0].events.length).to eq(2)
-    #   expect(court_cases[1].events.length).to eq(1)
-    # end
+  describe 'parse_charges' do
+    let(:charge_details) { cook_json['CaseDetails'][0]['ChargeDetails'] }
+    it 'creates a charge for every unique sequence in ChargeDetails' do
+      charges = subject.parse_charges(charge_details, case_number: '00CR1234567')
+
+      expect(charges.length).to eq(2)
+      expect(charges.all?{|c| c.type == :charge}).to eq(true)
+
+      expect(charges[0].index).to eq('1')
+      expect(charges[0].case_number).to eq('00CR1234567')
+      expect(charges[0].dcn).to eq('0123456789')
+      expect(charges[0].code).to eq('VOP')
+      expect(charges[0].description).to eq('Violation of Probation - Office Use Only')
+      expect(charges[0].offense_type).to eq('Violation')
+      expect(charges[0].offense_class).to eq(nil)
+      expect(charges[0].dispositions.length).to eq(1)
+      expect(charges[0].dispositions.first.description).to eq(nil)
+      expect(charges[0].dispositions.first.date).to eq(Date.parse('2000-10-18'))
+      expect(charges[0].dispositions.first.sentence_description).to eq('Probation')
+      expect(charges[0].dispositions.first.sentence_duration).to eq('0')
+
+      expect(charges[1].index).to eq('6')
+      expect(charges[1].case_number).to eq('00CR1234567')
+      expect(charges[1].dcn).to eq('0123456789')
+      expect(charges[1].code).to eq('720-570/402(C)')
+      expect(charges[1].description).to eq('POSS AMT CON SUB EXCEPT(A)/(D)')
+      expect(charges[1].offense_type).to eq('Felony')
+      expect(charges[1].offense_class).to eq('4')
+      expect(charges[1].dispositions.length).to eq(3)
+      expect(charges[1].dispositions.first.description).to eq('Finding of Guilty')
+      expect(charges[1].dispositions.first.date).to eq(Date.parse('2000-10-18'))
+      expect(charges[1].dispositions.first.sentence_description).to eq('Probation')
+      expect(charges[1].dispositions.first.sentence_duration).to eq('0')
+    end
   end
 end
